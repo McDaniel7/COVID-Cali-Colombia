c2dd71acbbbb6fb746568e60668a4c37736bcf9f
McDaniel7 <46606136+McDaniel7@users.noreply.github.com>
2021-02-24 00:43:21 +0800

    Update etas.py

--- etas.py
+++ etas.py
@@ -35,12 +35,13 @@ class StdDiffusionKernel(object):
                 ((torch.pow(delta_x, 2) / torch.pow(self.sigma_x, 2)) + \
                 (torch.pow(delta_y, 2) / torch.pow(self.sigma_y, 2))))
 
+
 class TorchETAS(torch.nn.Module):
     """
     PyTorch Module for ETAS
     """
 
-    def __init__(self, obs, region, n_MC = 10000, kernel = "StdDiffusion", data_dim = 3):
+    def __init__(self, obs, region, keep_latest_t = None, kernel = "StdDiffusion", data_dim = 3):
         """
         Denote the number of observations as N, data dim = 3(time, x, y)
 
@@ -51,20 +52,20 @@ class TorchETAS(torch.nn.Module):
         torch.nn.Module.__init__(self)
         # data
         self.obs    = torch.Tensor(np.array(obs)) # [ K, N ]
-        self.n = torch.max(self.obs[:, 0])
+        self.n = torch.max(self.obs[:, 0]) # max time stamp
         self.region = region
         # configurations
         self.N = obs.shape[0]
-        self.nMC = n_MC
+        self.kpt = keep_latest_t
         # parameters
         self.Lambda0   = torch.nn.Parameter(torch.Tensor(1).uniform_(0, .01)[0]) # [ 1 ]
-        self.C = torch.nn.Parameter(torch.Tensor(1).uniform_(0.5, 2)[0])
+        self.C = torch.nn.Parameter(torch.Tensor(1).uniform_(.01, 1)[0])
         self.Beta  = torch.nn.Parameter(torch.Tensor(1).uniform_(1, 3)[0])           # [ 1 ]
-        self.Sigmax = torch.nn.Parameter(torch.Tensor(1).uniform_(0.5, 2)[0]) # [ 1 ]
-        self.Sigmay = torch.nn.Parameter(torch.Tensor(1).uniform_(0.5, 2)[0]) # [ 1 ]
+        self.Sigmax = torch.nn.Parameter(torch.Tensor(1).uniform_(.01, .1)[0]) # [ 1 ]
+        self.Sigmay = torch.nn.Parameter(torch.Tensor(1).uniform_(.01, .1)[0]) # [ 1 ]
         if kernel == "StdDiffusion": self.kernel = StdDiffusionKernel(C=self.C, beta=self.Beta,
                                                                       sigma_x=self.Sigmax, 
-                                                                      sigma_y=self.Sigmay)  # useless
+                                                                      sigma_y=self.Sigmay)
     
     def _Lambda0(self, _t, _s):
         """
@@ -72,30 +73,11 @@ class TorchETAS(torch.nn.Module):
         """
         return self.Lambda0
     
-    def _MC_sample(self):
+    def _exp_kernel(self):
         """
-        Monte Carlo Sampling to estimate the integral term in log-likelihood
-
-        Return:
-        - lam: a lambda value at time `t`, location `s`
+        Exponential kernel
         """
-        m = 0
-        smp = []
-        x_left = self.region.bounds[0]
-        x_right = self.region.bounds[2]
-        y_lower = self.region.bounds[1]
-        y_upper = self.region.bounds[3]
-        while m < self.nMC:
-            xs = torch.Tensor(int(self.nMC / 2)).uniform_(x_left, x_right)
-            ys = torch.Tensor(int(self.nMC / 2)).uniform_(y_lower, y_upper)
-            points = MultiPoint(list(zip(xs, ys)))
-            tupVerts = self.region.exterior
-            p = Path(tupVerts)
-            if_cnt = p.contains_points(points)
-            smp.extend(list(zip(xs[if_cnt], ys[if_cnt])))
-            m += sum(if_cnt)
-        
-        return np.array(smp[:self.nMC])
+        return self.C * torch.exp(-self.Beta * torch.arange(1, self.n, 1))
 
     def _lambda(self, _t, _s):
         """
@@ -110,10 +92,15 @@ class TorchETAS(torch.nn.Module):
         if _t > 1:
             # self-exciting effect
             t = torch.ones(1) * _t
-            his_idx = self.obs[:, 0] < t
-            his_t = self.obs[his_idx, 0]
             s = torch.ones(2) * _s
-            his_s = self.obs[his_idx, 1:]
+            if self.kpt is not None:
+                his_idx = (self.obs[:, 0] >= t - self.kpt) & (self.obs[:, 0] < t)
+                his_t = self.obs[his_idx, 0]
+                his_s = self.obs[his_idx, 1:]
+            else:
+                his_idx = self.obs[:, 0] < t
+                his_t = self.obs[his_idx, 0]
+                his_s = self.obs[his_idx, 1:]
             ker = self.kernel.nu(t, s, his_t, his_s) # [ N(_t) ]
             lam    = self.Lambda0 + torch.sum(ker)    # [ 1 ]
         else:
@@ -134,54 +121,101 @@ class TorchETAS(torch.nn.Module):
         t = torch.ones(1) * _t
         
         if _t > 1:
-            his_idx = self.obs[:, 0] < t
-            his_t = self.obs[his_idx, 0]
-            his_s = self.obs[his_idx, 1:]
+            if self.kpt is not None:
+                his_idx = (self.obs[:, 0] >= t - self.kpt) & (self.obs[:, 0] < t)
+                his_t = self.obs[his_idx, 0]
+                his_s = self.obs[his_idx, 1:]
+            else:    
+                his_idx = self.obs[:, 0] < t
+                his_t = self.obs[his_idx, 0]
+                his_s = self.obs[his_idx, 1:]
             
             delta_t = t - his_t
-            M = self.C * torch.exp(-self.Beta * delta_t) / (self.Sigmax * self.Sigmay * 2 * np.pi * delta_t)
+            M = torch.div(self.C * torch.exp(-self.Beta * delta_t), self.Sigmax * self.Sigmay * 2 * np.pi * delta_t)
             
             delta_s = torch.Tensor(ps).unsqueeze(-1).expand(n_ps, 2, his_t.shape[0]) - his_s.T.expand(n_ps, 2, his_t.shape[0])
-            st1 = torch.div(torch.square(delta_s), torch.square(torch.stack([self.Sigmax, self.Sigmay], dim=0)).expand(his_t.shape[0], 2).T)
-            st2 = torch.exp(torch.div(torch.sum(st1, dim=1), -2 * delta_t))
+            st1 = torch.div(torch.square(delta_s),
+                            torch.square(torch.stack([self.Sigmax, self.Sigmay], dim=0)).expand(his_t.shape[0], 2).T * -2 * delta_t)
+            st2 = torch.exp(torch.sum(st1, dim=1))
             
             kers = torch.mm(st2, M.unsqueeze(-1))
             return kers
         else:
             return torch.ones(ps.shape[0]) * self.Lambda0
-        
-    def Int_Est(self):
+    
+    def _spa_tem_int(self):
         """
-        Integral term estimation in log-likelihood using Monte Carlo Sampling
+        Spatio-Temporal integration in log-likelihood, calculated in vector form
 
         Return:
-        - lam: Integral Estimation
+        - lam: Spatio-Temporal Integral estimation
         """
-        A = torch.ones(1)[0] * self.region.area
-        MC_points = self._MC_sample()
+        C = self.Lambda0 * self.region.area * self.n
+        exp_ker = self._exp_kernel()
         
-        lam_t = [torch.sum(self._lambda_t(_t, MC_points) * A) for _t in np.arange(1, self.n + 1, 1)]
-        lam_t = torch.stack(lam_t, dim = 0)
-        lam = torch.sum(lam_t) / self.nMC
+        def F_t(_t):
+            t = torch.ones(1) * _t
+            if _t > 1:
+                if self.kpt is not None:
+                    his_idx = (self.obs[:, 0] >= t - self.kpt) & (self.obs[:, 0] < t)
+                    his_t = self.obs[his_idx, 0]
+                else:    
+                    his_idx = self.obs[:, 0] < t
+                    his_t = self.obs[his_idx, 0]
+                    
+                delta_t = t - his_t
+                values, counts = torch.unique(delta_t, return_counts = True)
+            
+                return torch.sum(counts * exp_ker[:len(values)])
+            
+            else:
+                return torch.zeros(1)[0]
         
-        return lams2
+        F = [F_t(_t) for _t in np.arange(1, self.n + 1, 1)]
+        F = torch.sum(torch.stack(F, dim = 0))
+#         F = 0
+#         for _t in np.arange(1, self.n + 1, 1):
+#             F = F + F_t(_t)
         
+        lam = C + F
+        
+        return lam
+    
     def _log_likelihood(self):
         """
         Log likelihood function at time `T` = n
 
         Return:
         - loglik: a vector of log likelihood value at location k = 0, 1, ..., K [ K ]
-        - lams1: The sum term of history
-        - lams2: The integral term over spatio-temporal space
+        - lams:   
         """
+#         # lambda values from 0 to N
+#         lams0    = [ self._mu(t) for t in np.arange(self.N) ]     # ( N, [ K ] )
+#         lams1    = [ self._lambda(t) for t in np.arange(self.N) ] # ( N, [ K ] )
+#         lams0    = torch.stack(lams0, dim=1)                      # [ K, N ]
+#         lams1    = torch.stack(lams1, dim=1)                      # [ K, N ]
+#         Nloglams = self.obs * torch.log(lams0 + lams1 + 1e-5)     # [ K, N ]
+#         # log-likelihood function
+#         loglik   = (Nloglams - lams0 - lams1).sum()
 
-        lam_t_1 = [torch.sum(torch.log10(self._lambda_t(_t, self.obs[self.obs[:, 0] == _t, 1:]))) for _t in np.arange(1, self.n + 1, 1)]
-        lam_t_1 = torch.stack(lam_t_1, dim = 0)
-        lams1 = torch.sum(lam_t_1)
-        lams2 = self.Int_Est()
+        lam_t_1 = [torch.sum(torch.log(self._lambda_t(_t, self.obs[self.obs[:, 0] == _t, 1:]))) for _t in np.arange(1, self.n + 1, 1)]
+        lams1 = torch.sum(torch.stack(lam_t_1, dim = 0))
+    
+#         lams1 = 0
+#         for _t in np.arange(1, self.n + 1, 1):
+#             lams1 = lams1 + torch.sum(torch.log(self._lambda_t(_t, self.obs[self.obs[:, 0] == _t, 1:])))
         
-        loglik = lams1 + lams2
+        lams2 = self._spa_tem_int()
+    
+#         lams1 = 0
+#         A = torch.ones(1)[0] * self.region.area
+#         MC_points = self._MC_sample()
+#         for _t in np.arange(1, self.n + 1, 1):
+#             print(_t)
+#             lams1 += torch.sum(self._lambda_t(_t, MC_points) * A)
+#         lams1 = lams1 / self.nMC
+        
+        loglik = lams1 - lams2
 
         return loglik, lams1, lams2
 
@@ -191,70 +225,3 @@ class TorchETAS(torch.nn.Module):
         """
         # calculate data log-likelihood
         return self._log_likelihood()
-    
-    
-def train(model, niter=1000, lr=1e-1, log_interval=50):
-    """training procedure for one epoch"""
-    # coordinates of K locations
-    ##coords    = locs[:, :2]
-    # define model clipper to enforce inequality constraints
-    clipper1  = NonNegativeClipper()
-    ##clipper2  = ProximityClipper(coords, k=k)
-    # NOTE: gradient for loss is expected to be None, 
-    #       since it is not leaf node. (it's root node)
-    logliks = []
-    optimizer = optim.Adadelta(model.parameters(), lr=lr)
-    for _iter in range(niter):
-        try:
-            model.train()
-            optimizer.zero_grad()           # init optimizer (set gradient to be zero)
-            loglik, _, _ = model()
-            # objective function
-            loss         = - loglik
-            loss.backward()                 # gradient descent
-            optimizer.step()                # update optimizer
-            model.apply(clipper1)
-            ##model.apply(clipper2)
-            # log training output
-            logliks.append(loglik.item())
-            if _iter % log_interval == 0 and _iter != 0:
-                print("[%s] Train batch: %d\tLoglik: %.3e" % (arrow.now(), 
-                    _iter / log_interval, 
-                    sum(logliks) / log_interval))
-                logliks = []
-        except KeyboardInterrupt:
-            break
-
-
-
-class NonNegativeClipper(object):
-    """
-    References:
-    https://discuss.pytorch.org/t/restrict-range-of-variable-during-gradient-descent/1933
-    https://discuss.pytorch.org/t/set-constraints-on-parameters-or-layers/23620/3
-    """
-
-    def __init__(self):
-        pass
-
-    def __call__(self, module):
-        """enforce non-negative constraints"""
-        # TorchHawkes
-        if hasattr(module, 'Lambda0'):
-            Lambda0 = module.Lambda0.data
-            module.Lambda0.data = torch.clamp(Lambda0, min=0.)
-        if hasattr(module, 'Alpha'):
-            Alpha = module.Alpha.data
-            module.Alpha.data = torch.clamp(Alpha, min=0.)
-        if hasattr(module, 'Beta'):
-            Beta  = module.Beta.data
-            module.Beta.data  = torch.clamp(Beta, min=0.)
-        if hasattr(module, 'C'):
-            C  = module.C.data
-            module.C.data = torch.clamp(C, min=0.)
-        if hasattr(module, 'Sigmax'):
-            Sigmax  = module.Sigmax.data
-            module.Sigmax.data = torch.clamp(Sigmax, min=0.)
-        if hasattr(module, 'Sigmay'):
-            Sigmay  = module.Sigmay.data
-            module.Sigmay.data = torch.clamp(Sigmay, min=0.)
